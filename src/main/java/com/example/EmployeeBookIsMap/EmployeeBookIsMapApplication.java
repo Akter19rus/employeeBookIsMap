ea972bf84c4af73cde118f614d3258bcfc5f0719
Владислав <akter.res.hak@gmail.com>
2023-10-22 17:36:37 +0700

    master branch!

---
+++ src/main/java/com/example/EmployeeBookIsMap/EmployeeBookIsMapApplication.java
@@ -0,0 +1,13 @@
+package com.example.EmployeeBookIsMap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeBookIsMapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeBookIsMapApplication.class, args);
+	}
+
+}
